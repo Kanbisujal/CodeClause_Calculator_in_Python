8bc213cd8d698ee9bbf4daadd2aaf95d19cde209
Kanbi Sujal <131600057+Kanbisujal@users.noreply.github.com>
2023-05-20 17:22:52 +0530

    Calculator in Python.py

--- Calculator in Python.py
+++ Calculator in Python.py
@@ -1,52 +1,60 @@
-# define the basic arithmetic functions
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    return x / y
-
-# display the menu
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-print("0. Exit")
-
-# initialize the choice variable
-choice = None
-
-# loop until the user chooses to exit
-while choice != '0':
-    # get the user's choice
-    choice = input("Enter choice (1/2/3/4/0): ")
-    
-    # check if the user wants to exit
-    if choice == '0':
-        break
-    
-    # get the user's numbers
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-
-    # perform the selected operation
-    if choice == '1':
-        print(num1, "+", num2, "=", add(num1, num2))
-
-    elif choice == '2':
-        print(num1, "-", num2, "=", subtract(num1, num2))
-
-    elif choice == '3':
-        print(num1, "*", num2, "=", multiply(num1, num2))
-
-    elif choice == '4':
-        print(num1, "/", num2, "=", divide(num1, num2))
-
-    else:
-        print("Invalid input")
+from tkinter import *
+ 
+def iCalc(source, side):
+    storeObj = Frame(source, borderwidth=4, bd=4, bg="powder blue")
+    storeObj.pack(side=side, expand =YES, fill =BOTH)
+    return storeObj
+ 
+def button(source, side, text, command=None):
+    storeObj = Button(source, text=text, command=command)
+    storeObj.pack(side=side, expand = YES, fill=BOTH)
+    return storeObj
+ 
+class app(Frame):
+    def __init__(self):
+        Frame.__init__(self)
+        self.option_add('*Font', 'arial 20 bold')
+        self.pack(expand = YES, fill =BOTH)
+        self.master.title('Calculator')
+ 
+        display = StringVar()
+        Entry(self, relief=RIDGE, textvariable=display,
+          justify='right'
+          , bd=30, bg="powder blue").pack(side=TOP,
+                                          expand=YES, fill=BOTH)
+ 
+        for clearButton in (["C"]):
+            erase = iCalc(self, TOP)
+            for ichar in clearButton:
+                button(erase, LEFT, ichar, lambda
+                    storeObj=display, q=ichar: storeObj.set(''))
+ 
+        for numButton in ("789/", "456*", "123-", "0.+"):
+         FunctionNum = iCalc(self, TOP)
+         for iEquals in numButton:
+            button(FunctionNum, LEFT, iEquals, lambda
+                storeObj=display, q=iEquals: storeObj
+                   .set(storeObj.get() + q))
+ 
+        EqualButton = iCalc(self, TOP)
+        for iEquals in "=":
+            if iEquals == '=':
+                btniEquals = button(EqualButton, LEFT, iEquals)
+                btniEquals.bind('<ButtonRelease-1>', lambda e,s=self,
+                                storeObj=display: s.calc(storeObj), '+')
+ 
+ 
+            else:
+                btniEquals = button(EqualButton, LEFT, iEquals,
+                                    lambda storeObj=display, s=' %s ' % iEquals: storeObj.set
+                                    (storeObj.get() + s))
+ 
+    def calc(self, display):
+            try:
+                display.set(eval(display.get()))
+            except:
+                display.set("ERROR")
+ 
+ 
+if __name__=='__main__':
+ app().mainloop()
